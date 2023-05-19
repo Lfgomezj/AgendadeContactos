da56bd9f18dbc57b74cf94812dc3b72f73be7bac
Lfgomezj <123104490+Lfgomezj@users.noreply.github.com>
2023-05-19 07:47:43 -0500

    Se crean la pagina principal y los componentes

---
+++ Paginas/Contactos.jsx
@@ -0,0 +1,18 @@
+import Nav from "../Components/nav"
+import Card from "../Components/Card"
+import Card2 from "../Components/Card2"
+
+const Contactos = () => {
+	return (
+		<section className="contactos">
+			<Nav />
+			<section className="cards">
+
+				<Card />
+				<Card2 />
+			</section>
+		</section>
+	)
+}
+
+export default Contactos
---
+++ Paginas/Principal.jsx
@@ -0,0 +1,37 @@
+import Nav from "../Components/nav"
+import banner from "../assets/banner.jpg"
+import Card from "../Components/Card3"
+const Principal = () => {
+	return (
+
+		<section>
+			<Nav />
+			<section className="banner" >
+				<img src={banner} height="400"></img>
+			</section>
+			<section className="columnas">
+				<section className="texto">
+					<h1>Lista de Contactos</h1>
+					<section className="texto" id="text1">
+
+
+
+
+
+						<p> La aplicación de agenda de contactos es ser una herramienta imprescindible para mantener mi vida organizada y mis contactos al alcance de mi mano. Esta app, sin duda alguna, se ha convertido en mi compañero digital de confianza cuando se trata de gestionar mi lista de contactos de manera eficiente.          ​</p>
+
+					</section>
+				</section>
+
+				<Card />
+			</section>
+		</section>
+
+
+
+
+
+	)
+}
+
+export default Principal
