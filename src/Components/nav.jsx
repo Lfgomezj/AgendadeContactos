dbb8c2226b24bac749725bca92b79d00b10102be
Lfgomezj <123104490+Lfgomezj@users.noreply.github.com>
2023-05-19 08:38:40 -0500

    Se agregan estilos y los json

---
+++ src/Components/nav.jsx
@@ -0,0 +1,20 @@
+
+
+const nav = () => {
+	return (
+		<section className="navegador">
+			<ul className="my-menu">
+				<li><a className="navbar-brand" href="/"><a > AGENDA DE CONTACTOS</a></a></li>
+
+
+
+				<li>	<a><a href="/">INICIO</a></a></li>
+
+				<li><a><a href="/contactos">CONTACTOS</a></a></li>
+
+			</ul>
+		</section >
+	)
+}
+
+export default nav
